72520261fa72ca7b285893d156cfd3fd34301e42
Josh <gdwtaylor@gmail.com>
2025-08-14 23:16:53 -0400

    Add enum to represent boolean list shortcuts

---
+++ LibARMP/Enums/ColumnShortcutType.cs
@@ -0,0 +1,26 @@
+﻿namespace LibARMP
+{
+    /// <summary>
+    /// How column pointer tables are used to shortcut data storage (perhaps only boolean).
+    /// </summary>
+    /// <remarks><para></para>Only relevant for StorageMode.Column.</para></remarks>
+    public enum ColumnShortcutType
+    {
+        /// <summary>
+        /// All rows shortcut to true in this column.
+        /// </summary>
+        /// <remarks><para></para>For boolean type only.</para></remarks>
+        BoolAllTrue = -1,
+
+        /// <summary>
+        /// All rows shortcut to false in this column.
+        /// </summary>
+        /// <remarks><para></para>For boolean type only.</para></remarks>
+        BoolAllFalse = 0,
+
+        /// <summary>
+        /// The column does not shortcut and has normal value retrieval via pointer.
+        /// </summary>
+        None = 1,
+    }
+}
